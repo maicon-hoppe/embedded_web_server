a7a5dd1034a656db892b7afb00c9ab8a4cae7a36
maicon-hoppe <99830251+maicon-hoppe@users.noreply.github.com>
2026-04-21 02:11:50 -0300

    added indexeddb
    Signed-off-by: maicon-hoppe <99830251+maicon-hoppe@users.noreply.github.com>

--- upload_files/index_js.h
+++ upload_files/index_js.h
@@ -2,53 +2,103 @@
 #define UNIQUE_INDEX_JS
 
 const static char index_js_str[] = R"EOF(
-  // DBOpenRequest = indexedDB.open("br_population", 1);
+const lineChartXLabels = [];
+const lineChartCoordinates = [];
 
-  let timeSeries;
-  window.onload = async () =>
-  {
-      const BRPopulation = await fetch("https://servicodados.ibge.gov.br/api/v1/paises/BR/indicadores/77849");
-      const BRPopulationJson = await BRPopulation.json();
-      timeSeries = BRPopulationJson[0].series[0].serie;
+const DBOpenRequest = indexedDB.open("br_population", 1);
 
-      const lineChartXLabels = [];
-      const lineChartCoordinates = [];
-      const barChartXLabels = [];
-      const barChartCoordinates = [];
-      for (const timeData of timeSeries)
-      {
-          const timeDataEntry = Object.entries(timeData)[0];
-          if (timeDataEntry[1])
-          {
-              lineChartXLabels.push(timeDataEntry[0]);
-              lineChartCoordinates.push({ x: timeSeries.indexOf(timeData)+1, y: +timeDataEntry[1] });
-              // barChartXLabels.push(timeDataEntry[0]);
-              // barChartCoordinates.push({ x: timeSeries.indexOf(timeData)+1, y: +timeDataEntry[1] });
-          }
-      }
+DBOpenRequest.onsuccess = async (event) =>
+{
+    const db = DBOpenRequest.result;
 
-      const lineChart = document.createElement("line-chart");
-      lineChart.dataset.title = "OLÁ MUNDO";
-      lineChart.dataset.displayPoints = "false";
-      lineChart.dataset.chartData = JSON.stringify({
-          labels: lineChartXLabels,
-          datasets: [
-              {
-                  label: "População",
-                  data: lineChartCoordinates,
-              },
-          ],
-      });
+    const BRPopulation = await fetch("https://servicodados.ibge.gov.br/api/v1/paises/BR/indicadores/77849");
+    const BRPopulationJson = await BRPopulation.json();
 
-    //   const barChart = document.createElement("bar-chart");
-    //   barChart.dataset.title = "DADOS";
-    //   barChart.dataset.xLabels = JSON.stringify(barChartXLabels);
-    //   barChart.dataset.yLabel = "Crescimento Populacional";
-    //   barChart.dataset.coordinates = JSON.stringify(barChartCoordinates);
+    const dbPopulationTransaction = db.transaction(["br_population"], "readwrite");
+    const objectStoreCountRequest = dbPopulationTransaction.objectStore("br_population").count();
+    objectStoreCountRequest.onsuccess = () =>
+    {
+        console.log(`CONTAGEM: ${objectStoreCountRequest.result}`);
+        if (objectStoreCountRequest.result === 0)
+        {
+            const timeSeries = BRPopulationJson[0].series[0].serie;
+            console.log("SERIES -> ");
+            console.log(timeSeries);
 
-      document.body.appendChild(lineChart);
-    //   document.body.appendChild(barChart);
-  }
+            const populationObjectStore = dbPopulationTransaction.objectStore("br_population");
+
+            for (const timeCoordinate of timeSeries)
+            {
+                const [year, quantity] = Object.entries(timeCoordinate)[0];
+
+                if (quantity)
+                {
+                    console.log("Entrada ->");
+                    console.table({ year: +year, quantity: +quantity });
+
+                    const insertionRequest = populationObjectStore.add({ year: year, quantity: +quantity });
+                    insertionRequest.onsuccess = (event) =>
+                    {
+                        console.log("Value inserted in the database");
+                    };
+
+                    insertionRequest.onerror = (event) =>
+                    {
+                        console.log("Unable to add value");
+                        console.table({ year: +year, quantity: +quantity });
+                    }
+                }
+            }
+        }
+    }
+
+    const objectStore = db.transaction("br_population", "readonly").objectStore("br_population");
+    objectStore.openCursor().onsuccess = (event) =>
+    {
+        const cursor = event.target.result;
+        if (!cursor)
+        {
+            console.log("All data retrived");
+
+            console.log(`Chart labels: ${lineChartXLabels}`);
+            console.log("Coordenadas =>");
+            console.table(lineChartCoordinates);
+
+            const lineChart = document.createElement("line-chart");
+            lineChart.dataset.title = "OLÁ MUNDO";
+            lineChart.dataset.displayPoints = "false";
+            lineChart.dataset.chartData = JSON.stringify({
+                labels: lineChartXLabels,
+                datasets: [
+                    {
+                        label: "População",
+                        data: lineChartCoordinates,
+                    },
+                ],
+            });
+            document.body.appendChild(lineChart);
+
+            return;
+        }
+
+        const { year, quantity } = cursor.value;
+        lineChartXLabels.push(year);
+        lineChartCoordinates.push({ x: cursor.key, y: quantity });
+
+        cursor.continue();
+    };
+};
+
+DBOpenRequest.onupgradeneeded = async (event) =>
+{
+    const db = event.target.result;
+
+    db.onerror = () => { console.log("Cannot create database"); }
+
+    const BRPopulationObjectStore = db.createObjectStore("br_population", { autoIncrement: true });
+    BRPopulationObjectStore.createIndex("year", "year", { unique: true });
+    BRPopulationObjectStore.createIndex("quantity", "quantity");
+};
 )EOF";
 
 #endif
